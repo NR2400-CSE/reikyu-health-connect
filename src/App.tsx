b43c4fb96d40733f39c857ccc9de6b6348e4cebe
NR2400-CSE <namanrajcse@gmail.com>
2025-09-23 02:20:46 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import ChatbotPage from "./pages/ChatbotPage";
+import Index from "./pages/Index";               // Your homepage
+import ChatbotPage from "./pages/ChatbotPage";   // Chatbot interface
 import FeaturesPage from "./pages/FeaturesPage";
 import DashboardPage from "./pages/DashboardPage";
 import AboutPage from "./pages/AboutPage";
@@ -19,12 +19,18 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
+          {/* Homepage */}
           <Route path="/" element={<Index />} />
+
+          {/* Chatbot page */}
           <Route path="/chatbot" element={<ChatbotPage />} />
+
+          {/* Other pages */}
           <Route path="/features" element={<FeaturesPage />} />
           <Route path="/dashboard" element={<DashboardPage />} />
           <Route path="/about" element={<AboutPage />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+
+          {/* Catch-all 404 page */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
