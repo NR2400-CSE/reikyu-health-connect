b30b65ae41d4be28a1c50d664799e6860d9dfbf1
NR2400-CSE <namanrajcse@gmail.com>
2025-09-23 00:26:04 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,21 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
 import ChatbotPage from "./pages/ChatbotPage";
 import FeaturesPage from "./pages/FeaturesPage";
 import DashboardPage from "./pages/DashboardPage";
 import AboutPage from "./pages/AboutPage";
 import NotFound from "./pages/NotFound";
+import { useEffect } from "react";
+
+// Component to redirect to Google
+const RedirectToGoogle = () => {
+  useEffect(() => {
+    window.open("https://www.google.com", "_blank"); // opens Google in new tab
+  }, []);
+
+  return <div>Redirecting to Google...</div>;
+};
 
 const queryClient = new QueryClient();
 
@@ -19,12 +28,12 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
+          {/* Homepage now opens Google */}
+          <Route path="/" element={<RedirectToGoogle />} />
           <Route path="/chatbot" element={<ChatbotPage />} />
           <Route path="/features" element={<FeaturesPage />} />
           <Route path="/dashboard" element={<DashboardPage />} />
           <Route path="/about" element={<AboutPage />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
